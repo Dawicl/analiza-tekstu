031c56050f4810098045f42bbba67568f306681a
Dawicl <dawid.pawlak3720@gmail.com>
2023-03-23 19:22:48 +0100

    analiza-tekstu

--- 1.py
+++ 1.py
@@ -1,4 +1,21 @@
 plik = open("tekst.txt", "r", encoding="utf-8")
+plik2 = open ("wynik.txt", "w", encoding="utf-8")
 
-a = plik.readlines()
-print(a)
+alfabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ"
+
+alfabet1 = plik.read()
+alfabet1 = alfabet1.upper()
+
+for litera in alfabet:
+    for litera1 in alfabet1:
+        a = alfabet1.count(litera)
+        plik2.write(litera)
+        plik2.write("-")
+        plik2.write(str(a))
+        plik2.write(" ")
+        break
+    
+wynik2 = str(len(alfabet1))  
+plik2.write("\nCały tekst zawiera ") 
+plik2.write(wynik2)
+plik2.write(" znaków")
