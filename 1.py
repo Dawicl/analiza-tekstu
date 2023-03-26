d4edaa17fca326a2a8303a874212b391d10cbf94
Dawicl <dawid.pawlak3720@gmail.com>
2023-03-26 16:38:43 +0200

    analiza-tekstu_v3

--- 1.py
+++ 1.py
@@ -3,19 +3,28 @@ plik2 = open ("wynik.txt", "w", encoding="utf-8")
 
 alfabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ"
 
-alfabet1 = plik.read()
-alfabet1 = alfabet1.upper()
+tekst = plik.read()
+tekst = tekst.upper()
 
 for litera in alfabet:
-    for litera1 in alfabet1:
-        a = alfabet1.count(litera)
-        plik2.write(litera)
-        plik2.write("-")
-        plik2.write(str(a))
-        plik2.write(" ")
-        break
-    
-wynik2 = str(len(alfabet1))  
+    ilosc_znakow = tekst.count(litera)
+    plik2.write(litera)
+    plik2.write("-")
+    plik2.write(str(ilosc_znakow))
+    plik2.write(" - jest to ")
+    tekst_bezspacji = tekst.replace(" ", "") 
+    dzielnik = int(len(tekst_bezspacji))
+    procent = ilosc_znakow / dzielnik
+    procent = round(procent, 2)
+    plik2.write(str(procent))
+    plik2.write("% znaków w tekście.")
+    plik2.write("\n")
+
+usuwanie_spacji = tekst.replace(" ", "")    
+zliczanie_znakow = str(len(usuwanie_spacji))  
 plik2.write("\nCały tekst zawiera ") 
-plik2.write(wynik2)
+plik2.write(zliczanie_znakow)
 plik2.write(" znaków")
+
+plik.close
+plik2.close
--- 3.py
+++ 3.py
@@ -1,6 +1,12 @@
 plik = open("tekst.txt", "r", encoding="utf-8")
 
-litera = plik.read()
-litera = litera.upper()
-litera = litera.replace("A", "")
-print(litera)
+
+tekst = plik.read()
+usuwanie_litery = tekst.upper()
+nowy_tekst = usuwanie_litery.replace("A", "")
+nowy_tekst = nowy_tekst.capitalize()
+
+plik = open("tekst.txt", "w", encoding="utf-8")
+plik.write(nowy_tekst)
+
+plik.close
